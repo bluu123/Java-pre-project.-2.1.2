c929c01e41068450a44a37706fa9165830c7ee2a
Bluu123 <79271872363rus@gmail.com>
2022-08-19 17:54:13 +0300

    init

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,12 +1,21 @@
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+
 public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
+                applicationContext.getBean("helloworld", HelloWorld.class);
+        HelloWorld bean2 =
+                applicationContext.getBean("helloworld", HelloWorld.class);
+        Cat bean3 =
+                applicationContext.getBean("cat", Cat.class);
+        Cat bean4 =
+                applicationContext.getBean("cat", Cat.class);
         System.out.println(bean.getMessage());
+        System.out.println(bean.equals(bean2));
+        System.out.println(bean3.equals(bean4));
     }
 }
